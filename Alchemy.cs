72678400c260a619c8621ecbb9764155e8027956
ConnorRowe <connormagson@hotmail.co.uk>
2020-07-03 02:39:26 +0100

    Added a background and border to the ItemButtons.

--- Alchemy.cs
+++ Alchemy.cs
@@ -17,10 +17,14 @@ public class Alchemy : Node2D
         Crush
     }
 
+    private AlchemyStage _alchemyStage = AlchemyStage.MortarPestle;
+
     private Mortar _mortar;
     private PotionCircle _potionCircle;
     private HBoxContainer _reagentBox;
     private System.Collections.Generic.List<Item> _itemList = new System.Collections.Generic.List<Item>();
+
+    private Texture _itemBtnBg;
 
     public override void _Ready()
     {
@@ -28,10 +32,12 @@ public class Alchemy : Node2D
         _potionCircle = GetNode<PotionCircle>("PotionCircle");
         _reagentBox = GetNode<HBoxContainer>("ReagentsBox");
         _mortar.Alchemy = this;
+        _itemBtnBg = GD.Load<Texture>("res://textures/item_slot.png");
 
         _itemList.Add(Items.FLY_AGARIC);
         _itemList.Add(Items.ORPIMENT);
         _itemList.Add(Items.HOLLY_BERRIES);
+        _itemList.Add(Items.ELDERBERRIES);
 
         CreateReagentButtons();
     }
@@ -54,11 +60,46 @@ public class Alchemy : Node2D
             _reagentBox.AddChild(buttonControl);
             newItemButton.Alchemy = this;
             newItemButton.Item = i;
+            Sprite buttonBg = new Sprite();
+            buttonBg.Texture = _itemBtnBg;
+            buttonBg.Offset = new Vector2(7f, 8f);
+            buttonBg.ZIndex = -1;
+            newItemButton.AddChild(buttonBg);
         }
+        Sprite closingSprite = new Sprite();
+        Control endControl = new Control();
+        endControl.RectMinSize = new Vector2(32f, 32f);
+        closingSprite.Texture = _itemBtnBg;
+        closingSprite.Offset = new Vector2(-1, 8f);
+        closingSprite.RegionEnabled = true;
+        closingSprite.RegionRect = new Rect2(0f, 0f, 2f, 20f);
+        closingSprite.Scale = Vector2.One * 2;
+        _reagentBox.AddChild(endControl);
+        endControl.AddChild(closingSprite);
     }
 
     public void ItemButtonReleased(ItemButton btn)
     {
+        switch (_alchemyStage)
+        {
+            case AlchemyStage.MortarPestle:
+                {
+                    break;
+                }
+            case AlchemyStage.Calcinator:
+                {
+                    break;
+                }
+            case AlchemyStage.Alembic:
+                {
+                    break;
+                }
+            case AlchemyStage.Retort:
+                {
+                    break;
+                }
+        }
+
         GD.Print("Selected: " + btn.Item.Name);
     }
 }
--- Items.cs
+++ Items.cs
@@ -7,5 +7,9 @@ public class Items : Reference
     public static Item FLY_AGARIC = new Item("Fly Agaric", Item.ItemType.Reagent, Item.Rarity.Uncommon, "fly_agaric", Colors.LightPink);
     public static Item ORPIMENT = new Item("Orpiment", Item.ItemType.Reagent, Item.Rarity.Rare, "orpiment", Colors.Orange);
     public static Item HOLLY_BERRIES = new Item("Holly Berries", Item.ItemType.Reagent, Item.Rarity.Common, "holly_berries", Colors.IndianRed);
+    public static Item BRIMSTONE = new Item("Brimstone", Item.ItemType.Reagent, Item.Rarity.Common, "brimstone", Colors.LightGoldenrod);
+    public static Item AQUA_FORTIS = new Item("Aqua Fortis", Item.ItemType.Solvent, Item.Rarity.Uncommon, "aqua_fortis", Colors.PaleGoldenrod);
+    public static Item AQUA_VITAE = new Item("Aqua Vitae", Item.ItemType.Solvent, Item.Rarity.Common, "aqua_vitae", Colors.Snow);
+    public static Item ELDERBERRIES = new Item("Elderberries", Item.ItemType.Reagent, Item.Rarity.Common, "elderberries", Colors.RebeccaPurple);
 
 }
