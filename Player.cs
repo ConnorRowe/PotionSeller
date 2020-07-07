d37730566d51f19e44f9bb3a1b0c8e1d4f98ddeb
ConnorRowe <connormagson@hotmail.co.uk>
2020-07-07 21:41:05 +0100

    Gave the player normal maps.
    Had to implement a different animation method as AnimatedSprite has no support for normal maps.

--- Player.cs
+++ Player.cs
@@ -11,13 +11,25 @@ public class Player : KinematicBody2D
         Up
     }
 
+    // Nodes
     private JoystickButton _joystick = null;
-    private AnimatedSprite _playerSprite = null;
     private Direction _playerDirection = Direction.Right;
+    private Sprite _playerSprite;
 
+    // Assets
+    private Texture _runForwards;
+    private Texture _runForwardsNormal;
+    private Texture _runBackwards;
+    private Texture _runBackwardsNormal;
+    private Texture _runRight;
+    private Texture _runRightNormal;
+
+    // Gameplay vars
     private const float _MaxSpeed = 100f;
     private const float _Acceleration = 1000f;
     private Vector2 _motion = Vector2.Zero;
+    private const float _SpriteFPS = 8f;
+    private bool _playerIsMoving = false;
 
     public Direction PlayerDirection
     {
@@ -34,8 +46,21 @@ public class Player : KinematicBody2D
 
     public override void _Ready()
     {
+        // Get nodes
         _joystick = GetParent().GetNodeOrNull("CanvasLayer").GetChild(0).GetChild<JoystickButton>(0);
-        _playerSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        _playerSprite = GetNode<Sprite>("PlayerSprite");
+
+        // Load assets
+        _runForwards = GD.Load<Texture>("res://textures/Player_run_forwards.png");
+        _runBackwards = GD.Load<Texture>("res://textures/Player_run_backwards.png");
+        _runRight = GD.Load<Texture>("res://textures/Player_run_right.png");
+        _runForwardsNormal = GD.Load<Texture>("res://textures/normal/Player_run_forwards_n.png");
+        _runBackwardsNormal = GD.Load<Texture>("res://textures/normal/Player_run_backwards_n.png");
+        _runRightNormal = GD.Load<Texture>("res://textures/normal/Player_run_right_n.png");
+
+        Timer spriteAnimTimer = GetNode<Timer>("SpriteAnimateTimer");
+        spriteAnimTimer.WaitTime = 1f / _SpriteFPS;
+        spriteAnimTimer.Connect("timeout", this, nameof(AnimateSprite));
     }
 
     public override void _PhysicsProcess(float delta)
@@ -61,13 +86,12 @@ public class Player : KinematicBody2D
             bool positive = Mathf.Sign(vertical ? _motion.y : _motion.x) > 0f;
 
             PlayerDirection = vertical ? (positive ? Direction.Down : Direction.Up) : (positive ? Direction.Right : Direction.Left);
-            _playerSprite.SpeedScale = 1.0f;
+            _playerIsMoving = true;
         }
         else
         {
-            _playerSprite.SpeedScale = 0.0f;
+            _playerIsMoving = false;
             _playerSprite.Frame = 0;
-
         }
     }
 
@@ -76,19 +100,23 @@ public class Player : KinematicBody2D
         switch (_playerDirection)
         {
             case Direction.Down:
-                _playerSprite.Animation = "run_towards";
+                _playerSprite.Texture = _runBackwards;
+                _playerSprite.NormalMap = _runBackwardsNormal;
                 _playerSprite.FlipH = false;
                 break;
             case Direction.Left:
-                _playerSprite.Animation = "run_right_left";
+                _playerSprite.Texture = _runRight;
+                _playerSprite.NormalMap = _runRightNormal;
                 _playerSprite.FlipH = true;
                 break;
             case Direction.Right:
-                _playerSprite.Animation = "run_right_left";
+                _playerSprite.Texture = _runRight;
+                _playerSprite.NormalMap = _runRightNormal;
                 _playerSprite.FlipH = false;
                 break;
             case Direction.Up:
-                _playerSprite.Animation = "run_away";
+                _playerSprite.Texture = _runForwards;
+                _playerSprite.NormalMap = _runForwardsNormal;
                 _playerSprite.FlipH = false;
                 break;
         }
@@ -112,6 +140,18 @@ public class Player : KinematicBody2D
         if (_motion.Length() > _MaxSpeed)
         {
             _motion = _motion.Clamped(_MaxSpeed);
+        }
+    }
+
+    private void AnimateSprite()
+    {
+        if (_playerIsMoving)
+        {
+            int i = _playerSprite.Frame;
+            i++;
+            if (i > 7)
+                i = 0;
+            _playerSprite.Frame = i;
         }
     }
 }
